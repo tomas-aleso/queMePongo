5be253470c33c625d9e6103c5a93b204c2ff0e0b
Alejandro Ismael Chani <alejandro.chani24@gmail.com>
2019-09-14 17:01:24 -0300

    Saco cosas de FluentAPI

--- QueMePongo/queMePongo/DB.cs
+++ QueMePongo/queMePongo/DB.cs
@@ -32,26 +32,6 @@ namespace QueMePongo
 
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<Usuario>()
-                .HasMany<Guardarropa>(u => u.Guardarropas)
-                .WithMany(g => g.Usuarios)
-                .Map(cs =>
-                {
-                    cs.MapLeftKey("id_usuario");
-                    cs.MapRightKey("id_guardarropa");
-                    cs.ToTable("guardarropaxusuario");
-                });
-
-           /* modelBuilder.Entity<Guardarropa>()
-                .HasMany<Prenda>(g => g.Prendas)
-                .WithMany(p => p.Guardarropas)
-                .Map(cd =>
-                {
-                    cd.MapLeftKey("id_guardarropa");
-                    cd.MapRightKey("id_prenda");
-                    cd.ToTable("prendaxguardarropa");
-                });*/
-
         }
 
         public void clear(DbSet dbSet)
